b333cc29d2c9e62a4ae0eb6bf64a5de0a4ed31ae
victorcastillojara <victorcastillo.and1@gmail.com>
2019-04-03 19:42:39 -0300

    'actualizado'

--- epe1/src/app/app-routing.module.ts
+++ epe1/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'acerca', loadChildren: './acerca/acerca.module#AcercaPageModule' },
   { path: 'asignatura32', loadChildren: './asignatura32/asignatura32.module#Asignatura32PageModule' },
   { path: 'asignatura64', loadChildren: './asignatura64/asignatura64.module#Asignatura64PageModule' },
   { path: 'asignatura96', loadChildren: './asignatura96/asignatura96.module#Asignatura96PageModule' },
   { path: 'asignatura128', loadChildren: './asignatura128/asignatura128.module#Asignatura128PageModule' },
+  { path: 'acerca', loadChildren: './acerca/acerca.module#AcercaPageModule' },
 ];
 
 @NgModule({
