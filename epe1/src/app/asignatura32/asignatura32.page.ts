506e2c497681393d5ce5cf79cd78ce7fc2740a35
ELH-006 <ELH-006>
2019-04-10 14:58:06 -0300

    up actualizado

--- epe1/src/app/asignatura32/asignatura32.page.ts
+++ epe1/src/app/asignatura32/asignatura32.page.ts
@@ -18,11 +18,13 @@ export class Asignatura32Page implements OnInit {
   eva2:any;
   sumadecimal:any;
   
+  
   async enviar(){
 
     //Formula sin examen
     let suma = ((this.epe1 * 0.1) + (this.epe2 * 0.2) + (this.epe3 * 0.3) + (this.eva1 * 0.2) + (this.eva2 * 0.2));
     this.sumadecimal= suma.toFixed(1);
+    
 
     const alert =  await this.alertController.create({
       header: 'Felicitaciones',
@@ -31,11 +33,25 @@ export class Asignatura32Page implements OnInit {
       buttons: ['OK']
     });
     
-    if(this.sumadecimal<5.5){
-      this.navCtrl.navigateForward(`/respuesta/${this.epe1}/${this.epe2}/${this.epe3}/${this.eva1}/${this.eva2}`);
-    }else{
+    if(this.epe1>=7 || this.epe2>=7 || this.epe3>=7){
+      const alert =  await this.alertController.create({
+        header: 'Error',
+        subHeader: 'chupalochupalochupalo',
+        message: 'Las notas no pueden ser superior a 7.0',
+        buttons: ['OK']
+      });
       await alert.present();
+
+    }else{
+      if(this.sumadecimal<5.5 || this.epe1<= 3.9 || this.epe2<= 3.9 || this.epe3<= 3.9){
+
+        this.navCtrl.navigateForward(`/respuesta/${this.epe1}/${this.epe2}/${this.epe3}/${this.eva1}/${this.eva2}`);
+      }else{
+        await alert.present();
+      }
+      
     }
+    
     
   }
 
