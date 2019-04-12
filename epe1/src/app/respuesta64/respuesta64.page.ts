8a650d7667004ce0df22ce890a1e7707a900f4df
victorcastillojara <victorcastillo.and1@gmail.com>
2019-04-12 12:19:54 -0400

    Formula nota necesaria en examen, formulario respuesta64

--- epe1/src/app/respuesta64/respuesta64.page.ts
+++ epe1/src/app/respuesta64/respuesta64.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { summaryFileName } from '@angular/compiler/src/aot/util';
-import { NavController } from '@ionic/angular';
+import { NavController, IonLabel } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 
 
@@ -12,6 +12,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Respuesta64Page implements OnInit {
 
+
   constructor(public ActivatedRoute: ActivatedRoute, public navCtrl: NavController, public alertController: AlertController) { }
 
   epe1n: any;
@@ -19,22 +20,27 @@ export class Respuesta64Page implements OnInit {
   epe3n: any;
   eva1n: any;
   eva2n: any;
-  eva3n:any;
-  examen:any;
-  mensaje:any;
+  eva3n: any;
+  examen: any;
+  mensaje: any;
   suma: any;
-  promeva:any;
-  sumadecimal:any;
-  evaaa:any;
-  asd:any;
-  
- 
+  promeva: any;
+  sumadecimal: any;
+  evaaa: any;
+  asd: any;
+  not_presentacion: any;
+  not_necesaria: any;
+  mensaje2: any;
+  not_necesariadecimal: any;
 
-  volver(){
+
+
+
+  volver() {
     this.navCtrl.navigateBack('asignatura64');
   }
 
-  async calcular(){
+  async calcular() {
     this.epe1n = this.ActivatedRoute.snapshot.paramMap.get('epe1');
     this.epe2n = this.ActivatedRoute.snapshot.paramMap.get('epe2');
     this.epe3n = this.ActivatedRoute.snapshot.paramMap.get('epe3');
@@ -43,40 +49,40 @@ export class Respuesta64Page implements OnInit {
     this.eva3n = this.ActivatedRoute.snapshot.paramMap.get('eva3');
 
     //Formula con examen
-    this.suma = ((this.epe1n * 0.07) + (this.epe2n * 0.14) + 
-    (this.epe3n * 0.21) + (this.eva1n * 0.07) + (this.eva2n * 0.07)+(this.eva3n*0.14)+(this.examen*0.3));
+    this.suma = ((this.epe1n * 0.07) + (this.epe2n * 0.14) +
+      (this.epe3n * 0.21) + (this.eva1n * 0.07) + (this.eva2n * 0.07) + (this.eva3n * 0.14) + (this.examen * 0.3));
 
-    this.sumadecimal= this.suma.toFixed(1);
+    this.sumadecimal = this.suma.toFixed(1);
 
     //alerta aprobado
-    const alert =  await this.alertController.create({
+    const alert = await this.alertController.create({
       header: 'Felicitaciones',
       subHeader: 'Aprobaste',
-      message: 'tu promedio es: '+this.sumadecimal,
+      message: 'tu promedio es: ' + this.sumadecimal,
       buttons: ['OK']
     });
     //alerta reprobado
-    const alert2 =  await this.alertController.create({
+    const alert2 = await this.alertController.create({
       header: 'Lo sentimos',
       subHeader: 'Reprobaste',
-      message: 'Tu promedio es: '+this.sumadecimal,
+      message: 'Tu promedio es: ' + this.sumadecimal,
       buttons: ['OK']
     });
-if(this.examen>7){
-  const alert3 =  await this.alertController.create({
-    header: 'Lo sentimos',
-    message: 'La nota no puede ser superior a 7.0',
-    buttons: ['OK']
-  });
-await alert3.present();
-}else{
-if(this.sumadecimal<4.0){
-  await alert2.present();
-    }else{
-      await alert.present();
+    if (this.examen > 7) {
+      const alert3 = await this.alertController.create({
+        header: 'Lo sentimos',
+        message: 'La nota no puede ser superior a 7.0',
+        buttons: ['OK']
+      });
+      await alert3.present();
+    } else {
+      if (this.sumadecimal < 4.0) {
+        await alert2.present();
+      } else {
+        await alert.present();
+      }
     }
-}
-    
+
 
 
   }
@@ -90,19 +96,34 @@ if(this.sumadecimal<4.0){
     this.eva3n = this.ActivatedRoute.snapshot.paramMap.get('eva3');
 
     //Formula sin examen
-    this.suma = ((this.epe1n * 0.1) + (this.epe2n * 0.2) + 
-    (this.epe3n * 0.3) + (this.eva1n * 0.1) + (this.eva2n * 0.1)+(this.eva3n*0.2));
+    this.suma = ((this.epe1n * 0.1) + (this.epe2n * 0.2) +
+      (this.epe3n * 0.3) + (this.eva1n * 0.1) + (this.eva2n * 0.1) + (this.eva3n * 0.2));
 
-    this.promeva=(this.eva1n * 0.1) + (this.eva2n * 0.1) + (this.eva3n * 0.2);
-    this.evaaa=((this.promeva)*0.6)/0.4;
-    this.asd=this.promeva+this.evaaa;
+    this.promeva = (this.eva1n * 0.1) + (this.eva2n * 0.1) + (this.eva3n * 0.2);
+    this.evaaa = ((this.promeva) * 0.6) / 0.4;
+    this.asd = this.promeva + this.evaaa;
 
 
-    this.sumadecimal= this.suma.toFixed(1);
-    if(this.sumadecimal<5.5 || this.evaaa < 4.0){
-      this.mensaje="Te fuiste a examen con un: "+this.sumadecimal;
+    //Formula nota para aprobar el examen
+    this.not_presentacion = (this.suma * 0.7);
+    this.not_necesaria = (4 - this.not_presentacion) / 0.3;
+
+    //nota para aprobar el examen con decimal
+    this.not_necesariadecimal = this.not_necesaria.toFixed(1);
+
+    //nota de presentacion a examen
+    this.sumadecimal = this.suma.toFixed(1);
+
+    if (this.sumadecimal < 5.5 || this.evaaa < 4.0) {
+      this.mensaje = "Te fuiste a examen con un: " + this.sumadecimal;
+
+      //condición si nota necesaria es superior a 7 reprueba el ramo.
+      if (this.not_necesariadecimal > 7.0) {
+        this.mensaje2 = "Reprobaste el ramo, necesitas un: " + this.not_necesariadecimal;
+      } else {
+        this.mensaje2 = "necesitas un: " + this.not_necesariadecimal;
+      }
     }
-
   }
-
 }
+
